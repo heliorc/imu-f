567908987ce697c08c5ca57fe9cf597a0e912856
Tim Sweet <timothyswt@gmail.com>
2017-12-27 16:52:56 -0700

    formatting

--- src/target/default.h
+++ src/target/default.h
@@ -15,14 +15,14 @@
     #define GYRO_CS_GPIO      GPIOA
     #define GYRO_SPI          SPI2
 
-    #define SPI2_ENABLED 1
-    #define SPI2_HARDWARE_CS 0
-    #define SPI2_CS_PIN 0
-    #define SPI2_CS_PORT 0
-    #define SPI2_MISO_PIN GPIO_PIN_10
-    #define SPI2_MISO_PORT GPIOA
-    #define SPI2_MOSI_PIN GPIO_PIN_11
-    #define SPI2_MOSI_PORT GPIOA
-    #define SPI2_SCK_PIN GPIO_PIN_1
-    #define SPI2_SCL_PORT GPIOF
+    #define SPI2_ENABLED      1
+    #define SPI2_HARDWARE_CS  0
+    #define SPI2_CS_PIN       0
+    #define SPI2_CS_PORT      0
+    #define SPI2_MISO_PIN     GPIO_PIN_10
+    #define SPI2_MISO_PORT    GPIOA
+    #define SPI2_MOSI_PIN     GPIO_PIN_11
+    #define SPI2_MOSI_PORT    GPIOA
+    #define SPI2_SCK_PIN      GPIO_PIN_1
+    #define SPI2_SCL_PORT     GPIOF
 #endif
