22e7b0f04f04c72bd13dd968d66bf215fc103b08
Tim Sweet <timothyswt@gmail.com>
2017-12-27 16:42:32 -0700

    resolving merge conflict

--- src/stm32/stm32.h
+++ src/stm32/stm32.h
@@ -14,11 +14,7 @@ extern uint32_t InlineMicros(void);
 extern void InlineDelayUs(uint32_t uSec);
 extern void InlineDelayMs(uint32_t mSec);
 extern void inline_digital_hi(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
-<<<<<<< HEAD
-extern void InlineDigitalLo(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
-=======
 extern void inline_digital_lo(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
->>>>>>> 0b7ca046eb733b9335e68be2e0bf28e8a6a7e9b3
 extern int InlineIsPinStatusHi(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
 extern void DeInitGpio(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
 extern void init_gpio(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint32_t on);
