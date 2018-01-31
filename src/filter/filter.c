86829690768c403879b5bb58d5cee844c6d6275b
Tim Sweet <timothyswt@gmail.com>
2018-01-31 09:59:48 -0700

    fixing hard fault rewferences

--- src/filter/filter.c
+++ src/filter/filter.c
@@ -2,7 +2,7 @@
 #include "biquad.h"
 #include "fast_kalman.h"
 
-biquad_state_t *lpfFilterStateRate;
+biquad_state_t lpfFilterStateRate;
 
 void filter_data(axisData_t *gyroRateData, axisData_t *gyroAccData,float gyroTempData, filteredData_t *filteredData)
 {
@@ -12,22 +12,22 @@ void filter_data(axisData_t *gyroRateData, axisData_t *gyroAccData,float gyroTem
 	{
         firstRun = 0;
 		fast_kalman_init(3000.0f, 88.0f, 3000.0f, 0.0f, DISTANCE_ESTIMATION);
-		memset(&lpfFilterStateRate->x, 0, sizeof(biquad_axis_state_t));
-		memset(&lpfFilterStateRate->y, 0, sizeof(biquad_axis_state_t));
-		memset(&lpfFilterStateRate->z, 0, sizeof(biquad_axis_state_t));
+		memset(&(lpfFilterStateRate.x), 0, sizeof(biquad_axis_state_t));
+		memset(&(lpfFilterStateRate.y), 0, sizeof(biquad_axis_state_t));
+		memset(&(lpfFilterStateRate.z), 0, sizeof(biquad_axis_state_t));
         #ifndef DEBUG
-        biquad_init(150.0f, &(lpfFilterStateRate->x), 0.00003125f, FILTER_TYPE_LOWPASS, &(lpfFilterStateRate->x), BIQUAD_BANDWIDTH);
-        biquad_init(150.0f, &(lpfFilterStateRate->y), 0.00003125f, FILTER_TYPE_LOWPASS, &(lpfFilterStateRate->y), BIQUAD_BANDWIDTH);
-        biquad_init(150.0f, &(lpfFilterStateRate->z), 0.00003125f, FILTER_TYPE_LOWPASS, &(lpfFilterStateRate->z), BIQUAD_BANDWIDTH);
+        biquad_init(150.0f, &(lpfFilterStateRate.x), 0.00003125f, FILTER_TYPE_LOWPASS, &(lpfFilterStateRate.x), BIQUAD_BANDWIDTH);
+        biquad_init(150.0f, &(lpfFilterStateRate.y), 0.00003125f, FILTER_TYPE_LOWPASS, &(lpfFilterStateRate.y), BIQUAD_BANDWIDTH);
+        biquad_init(150.0f, &(lpfFilterStateRate.z), 0.00003125f, FILTER_TYPE_LOWPASS, &(lpfFilterStateRate.z), BIQUAD_BANDWIDTH);
         #endif
 	}
 	filteredData->rateData.x = fast_kalman_pdate(0, gyroRateData->x);
 	filteredData->rateData.y = fast_kalman_pdate(1, gyroRateData->y);
 	filteredData->rateData.z = fast_kalman_pdate(2, gyroRateData->z);
     #ifndef DEBUG
-	filteredData->rateData.x = biquad_update(filteredData->rateData.x, &(lpfFilterStateRate->x));
-	filteredData->rateData.y = biquad_update(filteredData->rateData.y, &(lpfFilterStateRate->y));
-	filteredData->rateData.z = biquad_update(filteredData->rateData.z, &(lpfFilterStateRate->z));
+	filteredData->rateData.x = biquad_update(filteredData->rateData.x, &(lpfFilterStateRate.x));
+	filteredData->rateData.y = biquad_update(filteredData->rateData.y, &(lpfFilterStateRate.y));
+	filteredData->rateData.z = biquad_update(filteredData->rateData.z, &(lpfFilterStateRate.z));
     #endif
 
 	//what's insie the filteredData_t typedef
--- src/filter/filter.h
+++ src/filter/filter.h
@@ -2,6 +2,6 @@
 #include "includes.h"
 #include "biquad.h"
 
-extern biquad_state_t *lpfFilterStateRate;
+extern biquad_state_t lpfFilterStateRate;
 
 extern void filter_data(axisData_t *gyroRateData, axisData_t *gyroAccData,float gyroTempData, filteredData_t *filteredData);
