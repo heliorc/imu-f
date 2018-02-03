a2fcc313e4ee0005c3f602226b20d53d1f3c4ace
Kalyn Doerr <kalyn.doerr@gmail.com>
2018-02-02 21:36:22 -0600

    remove old board_comm, rename new one

--- make.py
+++ make.py
@@ -170,7 +170,7 @@ def configure_target(TARGET):
         "src",
         os.path.join("src", "stm32"),
         os.path.join("src", "target"),
-        os.path.join("src", "new_board_comm"),
+        os.path.join("src", "board_comm"),
         LIBRARY_PATH + "/CMSIS_std/Device/ST/STM32F30x/Include",
         LIBRARY_PATH + "/STM32F30x_StdPeriph_Driver/inc",
         LIBRARY_PATH + "/CMSIS_std/Include"
@@ -180,7 +180,7 @@ def configure_target(TARGET):
         "src",
         os.path.join("src", "stm32"),
         os.path.join("src", "target"),
-        os.path.join("src", "new_board_comm"),
+        os.path.join("src", "board_comm"),
         LIBRARY_PATH + "/CMSIS_std/Device/ST/STM32F30x/Source",
         LIBRARY_PATH + "/STM32F30x_StdPeriph_Driver/src"
     ]
