53cde62768d7e112327086b45f12762f5afeaac4
Kalyn Doerr <kalyn.doerr@gmail.com>
2018-02-02 21:57:58 -0600

    add a bit more to gyro.c

--- src/new_gyro/gyro.c
+++ src/new_gyro/gyro.c
@@ -2,3 +2,8 @@
 #include "gyro.h"
 
 volatile int calibratingGyro;
+volatile axisData_t gyroSum;
+volatile axisData_t gyroCalibrationTrim;
+
+SPI_InitTypeDef gyroSpiInitStruct;
+DMA_InitTypeDef gyroDmaInitStruct;
