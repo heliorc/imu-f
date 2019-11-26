d496175a44cd4c4bd0f5312807f26c35a44bd731
Quick-Flash <46289813+Quick-Flash@users.noreply.github.com>
2019-11-26 12:09:14 -0700

    q multiplier change
    changing the multiplier for q. I have doubts on its usefulness.

--- src/filter/kalman.c
+++ src/filter/kalman.c
@@ -11,7 +11,7 @@ float      r_filter_weight = 1.0f;
 void init_kalman(kalman_t *filter, float q)
 {
     memset(filter, 0, sizeof(kalman_t));
-    filter->q = q * 0.001f;      //add multiplier to make tuning easier
+    filter->q = q * 0.0001f;      //add multiplier to make tuning easier
     filter->r = 88.0f;           //seeding R at 88.0f
     filter->p = 30.0f;           //seeding P at 30.0f
     filter->e = 1.0f;
@@ -93,7 +93,7 @@ inline float kalman_process(kalman_t* kalmanState, volatile float input, volatil
     } else {
         kalmanState->e = 1.0f;
     }*/
-    
+
     kalmanState->e = ABS((target - input) * 2) + ABS(input/5);
 
     //prediction update
